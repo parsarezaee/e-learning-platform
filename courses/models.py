f14b3e2ed0a2bf418108eff25a8853ad050c1cc1
parsarezaee <parsarezaee777@yahoo.com>
2022-05-06 21:40:11 +0430

    add ordering to module and content objects

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.utils import timezone
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -37,9 +38,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -53,8 +58,12 @@ class Content(models.Model):
                                          'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
 
+    class Meta:
+        ordering = ['order']
 
+        
 class BaseContent(models.Model):
     title = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
